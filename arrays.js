a3e884e40dc16ccfe8acda8dea62707c895caa15
Alex Sabo <djsabox@gmal.com>
2018-01-25 18:51:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,7 +17,6 @@ function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element);
   return array
 }
-function
-accessElementInArray(array, index) {
+function accessElementInArray(array, index) {
 array[index];
 }

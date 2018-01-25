69d3e8b1c7b6bf18a1d1bdd749d9f0e655ef113a
Alex Sabo <djsabox@gmal.com>
2018-01-25 19:08:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -38,5 +38,5 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  array.slice(0, array.lenght - 1)
+  array.slice(0, array.length - 1)
 }
